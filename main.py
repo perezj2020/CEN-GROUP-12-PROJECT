c37f8d491c2a9ba50b72422bfd301bfcb9609f14
James Merilien <88689946+Tile561@users.noreply.github.com>
2023-04-14 14:35:25 -0400

    Update main.py
    added a gui to the code

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ import random
 import wikipedia
 import webbrowser
 import pyjokes
+import tkinter as tk
+import threading
+from tkinter import scrolledtext
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import random
+import wikipedia
+import webbrowser
+import pyjokes
 import time 
 import wolframalpha
 from time import ctime
@@ -14,6 +24,10 @@ import smtplib
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+
+root = tk.Tk()
+root.geometry("400x400")
+root.title("Elara - Your Personal Assistant")
 
 def speak(text):
     engine.say(text)
@@ -29,11 +43,12 @@ def greet():
         speak("Good evening!")
     speak("My name is Elara. How can I help you today?")
 
-def get_command():
+def get_command(textbox):
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
         recognizer.adjust_for_ambient_noise(source)
         print("Listening...")
+        textbox.insert(tk.END, "Assistant: " + "Listening..." + "\n")
         audio = recognizer.listen(source)
         try:
             command = recognizer.recognize_google(audio).lower()
@@ -47,9 +62,9 @@ def open_website(url):
     webbrowser.open(url)
 
 def get_wikipedia_summary(topic):
-    summary = wikipedia.summary(topic, sentences=3)
-    print(summary)
-    speak(summary)
+    get_wikipedia_summary.summary = wikipedia.summary(topic, sentences=3)
+    print(get_wikipedia_summary.summary)
+    speak(get_wikipedia_summary.summary)
 
 def tell_joke():
     joke = pyjokes.get_joke()
@@ -60,25 +75,16 @@ def wolf():
     speak("Ask any question")
     question=get_command()
 
-    #question = input('Question: ')
     app_id = "5AKJW8-WRERQJRA54"
     client = wolframalpha.Client(app_id)
     res = client.query(question)
-    answer = next(res.results).text
-    speak(answer)
+    wolf.answer = next(res.results).text
+    speak(wolf.answer)
 
 def flip_coin():
     return random.choice(["You got Heads","You got Tails"])
-'''''''''
-def send_email(to, subject, body):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login('your_email@gmail.com', 'your_password')
-    message = f"Subject: {subject}\n\n{body}"
-    server.sendmail('your_email@gmail.com', to, message)
-    server.quit()
-'''''''''
-def write_note():
+
+def write_note(self):
     speak("What should I write down?")
     note_text = get_command()
     now = datetime.datetime.now()
@@ -119,94 +125,121 @@ def weather():
                 str(current_humidiy) +
                 "\n description = " +
                 str(weather_description))
-
-def main():
-    greet()
-    while True:
-        command = get_command()
-        if "open youtube" in command:
-            open_website("https://www.youtube.com/")
-            speak("Opening YouTube")
-        elif "open google" in command:
-            open_website("https://www.google.com/")
-            speak("Opening Google")
-        elif "news" in command:
-            open_website("https://news.google.com/home?hl=en-US&gl=US&ceid=US:en")
-            speak("Here are some headlines form Google News")
-        elif "open stack overflow" in command:
-            open_website("https://stackoverflow.com/")
-            speak("Opening Stack Overflow")
-        elif "open wikipedia" in command:
-            open_website("https://www.wikipedia.org/")
-            speak("Opening Wikipedia")
-        elif 'search'  in command:
-            command = command.replace("search", "")
-            webbrowser.open_new_tab(command)
-        	
-        elif "open facebook" in command:
-            open_website("https://www.facebook.com/")
-            speak("Opening Facebook")
-        elif "open twitter" in command:
-            open_website("https://twitter.com/home")
-            speak("Opening Twitter")
-        elif "open yahoo" in command:
-            open_website("https://www.yahoo.com/")
-            speak("Opening Yahoo")
-        elif "open amazon" in command:
-            open_website("https://www.amazon.com/")
-            speak("Opening Amazon")
-        elif "wikipedia" in command:
-            topic = command.replace("wikipedia", "")
-            get_wikipedia_summary(topic)
-        elif "joke" in command:
-            tell_joke()
-        elif "write a note" in command:
-            write_note()
-        elif "show the note" in command:
-            show_note()
-        elif "how are you" in command:
-            speak("I am well")
-        elif "what is your name" in command:
-            speak("My name is Elera")
+class GUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Elara Voice Assistant")
+        self.root.geometry("1000x1000")
+        self.textbox = scrolledtext.ScrolledText(self.root, height=20, width=80, font=("Times New Roman", 16, "bold"), fg="white", bg="gray25")
+        self.textbox.pack()
         
-        elif "music" in command:
-            open_website("https://www.youtube.com/live/jfKfPfyJRdk?feature=share")
-        elif "i love you" in command:
-            speak("Ok, dont care")
-        elif 'wikipedia' in command:
-            speak('Searching Wikipedia...')
-            command = command.replace("wikipedia", "")
-            results = wikipedia.summary(command, sentences = 3)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
-        elif "who created you" in command:
-            speak("Its a mystery lol")
-        elif "weather" in command:
-            weather()
-        elif "time" in command:
-            speak(ctime())
-        elif "exit" in command:
-            speak("Goodbye.")
-            break
-        elif "wolf" in command:
-            wolf()
-        elif "flip a coin" in command:
-            speak(flip_coin())
+        self.button = tk.Button(self.root, text="Click to Speak", command=self.start_assistant, font=("Helvetica", 18), fg="white", bg="DodgerBlue2", activebackground="DodgerBlue3")
+        self.button.pack()
 
+        self.root.mainloop()
+
+
+    def start_assistant(self):
+        self.button.config(text="Listening...")        
+        threading.Thread(target=self.run_assistant).start()
     
-        
-if __name__ == "__main__":
-    main()
+    def write_note(self):
+        speak("What should I write down?")
+        note_text = get_command(self.textbox)
+        now = datetime.datetime.now()
+        with open("notes.txt", "a") as file:
+            file.write(f"[{now.strftime('%Y-%m-%d %H:%M:%S')}] {note_text}\n")
+        speak("Note saved.")
 
-'''''''''''
-        elif "send email" in command:
-            speak("Who is the recipient?")
-            recipient = get_command()
-            speak("What is the subject?")
-            subject = get_command()
-            speak("What would you like to say?")
-            body = get_command()
-            send_email(recipient, subject, body)
-            speak("Email sent.")
-        '''''''''''
+    def run_assistant(self):
+        greet()
+        self.textbox.insert(tk.END, "Assistant: " + "My name is Elara. How can I help you today?" + "\n")
+        while True:
+            command = get_command(self.textbox)
+            self.textbox.insert(tk.END, f"You: {command}\n")
+            if "open youtube" in command:
+                open_website("https://www.youtube.com/")
+                speak("Opening YouTube")
+                self.textbox.insert(tk.END, "Assistant: " + "Opening Youtube" + "\n")
+            elif "open google" in command:
+                open_website("https://www.google.com/")
+                speak("Opening Google")
+                self.textbox.insert(tk.END, "Assistant: " + "Opening Google" + "\n")
+            elif "news" in command:
+                open_website("https://news.google.com/home?hl=en-US&gl=US&ceid=US:en")
+                speak("Here are some headlines from Google News")
+                self.textbox.insert(tk.END, "Assistant: " + "Here are some headlines from Google News" + "\n")
+            elif "open stack overflow" in command:
+                open_website("https://stackoverflow.com/")
+                speak("Opening Stack Overflow")
+                self.textbox.insert(tk.END, "Assistant: " + "Opening Stack Overflow" + "\n")
+            elif "open wikipedia" in command:
+                open_website("https://www.wikipedia.org/")
+                speak("Opening Wikipedia")
+                self.textbox.insert(tk.END, "Assistant: " + "Opening Wikipedia" + "\n")
+            elif 'search' in command:
+                command = command.replace("search", "")
+                webbrowser.open_new_tab(command)
+                self.textbox.insert(tk.END, f"Assistant: search{command}\n")
+
+            elif "open facebook" in command:
+                open_website("https://www.facebook.com/")
+                speak("Opening Facebook")
+                self.textbox.insert(tk.END, "Assistant: " + "Opening Facebook" + "\n")
+            elif "open twitter" in command:
+                open_website("https://twitter.com/home")
+                speak("Opening Twitter")
+                self.textbox.insert(tk.END, "Assistant: " + "Opening Twitter" + "\n")
+            elif "open yahoo" in command:
+                open_website("https://www.yahoo.com/")
+                speak("Opening Yahoo")
+                self.textbox.insert(tk.END, "Assistant: " + "Opening Yahoo" + "\n")
+            elif "open amazon" in command:
+                open_website("https://www.amazon.com/")
+                speak("Opening Amazon")
+                self.textbox.insert(tk.END, "Assistant: " + "Opening Amazon" + "\n")
+            elif "wikipedia" in command:
+                topic = command.replace("wikipedia", "")
+                get_wikipedia_summary(topic)
+                self.textbox.insert(tk.END, f"Assistant:{get_wikipedia_summary.summary} \n")
+            elif "joke" in command:
+                tell_joke()
+            elif "write a note" in command:
+                self.write_note()
+            elif "show the note" in command:
+                show_note()
+                file = open("notes.txt", "r")
+                self.textbox.insert(tk.END, f"Assistant:{file.read()} \n")
+            elif "how are you" in command:
+                speak("I am well")
+                self.textbox.insert(tk.END, "Assistant: " + "I am well" + "\n")
+            elif "what is your name" in command:
+                speak("My name is Elera")
+                self.textbox.insert(tk.END, "Assistant: " + "My name is Elera" + "\n")
+
+            elif "music" in command:
+                open_website("https://www.youtube.com/live/jfKfPfyJRdk?feature=share")
+                speak("Enjoy your music!")
+                self.textbox.insert(tk.END, "Assistant: " + "Enjoy your music!"+ "\n")
+            elif "coin" in command:
+                speak(flip_coin())
+            elif "wolf" in command:
+                wolf()
+                self.textbox.insert(tk.END, f"Assistant:{wolf.answer}  \n")
+            elif "time" in command:
+                speak(ctime())
+                self.textbox.insert(tk.END, f"Assistant:{ctime()}  \n")
+            elif "weather" in command:
+                weather()
+
+            elif "stop" in command or "exit" in command or "quit" in command:
+                speak("Goodbye!")
+                self.button.config(text="Start Listening")
+                self.textbox.insert(tk.END, "Assistant: " + "Goodbye" + "\n")
+                break
+            else:
+                speak("Sorry, I didn't understand that.")
+                self.textbox.insert(tk.END, "Assistant: " + "Sorry, I didn't understand that" + "\n")
+                  
+if __name__=='__main__':
+    gui=GUI()
